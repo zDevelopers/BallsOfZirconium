706052c6c52111da943f99afbd36671475d43a65
Amaury Carrade <amaury@carrade.eu>
2014-10-07 21:35:18 +0200

    Players cannot access the private chests of other teams.

--- src/main/java/eu/carrade/amaury/BallsOfSteel/BoSListener.java
+++ src/main/java/eu/carrade/amaury/BallsOfSteel/BoSListener.java
@@ -4,13 +4,19 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.Action;
+
+import eu.carrade.amaury.BallsOfSteel.i18n.I18n;
 
 public class BoSListener implements Listener {
 	
 	private BallsOfSteel p = null;
+	private I18n i = null;
 	
 	public BoSListener(BallsOfSteel plugin) {
 		this.p = plugin;
+		this.i = p.getI18n();
 	}
 	
 	/**
@@ -34,6 +40,30 @@ public class BoSListener implements Listener {
 	public void onBlockBurn(BlockBurnEvent ev) {
 		if(p.getGameManager().getTrackedChests().contains(ev.getBlock().getLocation())) {
 			ev.setCancelled(true);
+		}
+	}
+	
+	/**
+	 * Used to prevent player to access the chest of another team.
+	 * 
+	 * @param ev
+	 */
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent ev) {
+		
+		if(ev.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+		if(!BoSUtils.isSharedChest(ev.getClickedBlock())) return;
+		
+		BoSTeam playerTeam = p.getTeamManager().getTeamForPlayer(ev.getPlayer());
+		
+		if(p.getGameManager().getTrackedChests().contains(ev.getClickedBlock().getLocation())
+				&& (playerTeam == null
+					|| (!ev.getClickedBlock().getLocation().equals(playerTeam.getChestLocation1())
+					    && !ev.getClickedBlock().getLocation().equals(playerTeam.getChestLocation2())))) {
+			
+			ev.getPlayer().sendMessage(i.t("chests.otherTeamChest"));
+			ev.setCancelled(true);
+			
 		}
 	}
 }
