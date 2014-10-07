3b088b89803a15aa9096da65cb634b004836e1d6
Amaury Carrade <amaury@carrade.eu>
2014-10-07 22:25:02 +0200

    Feature: players cannot place items in/remove items from the diamond chests using hoppers.

--- src/main/java/eu/carrade/amaury/BallsOfSteel/BoSListener.java
+++ src/main/java/eu/carrade/amaury/BallsOfSteel/BoSListener.java
@@ -1,14 +1,16 @@
 package eu.carrade.amaury.BallsOfSteel;
 
-import java.util.List;
-
+import org.bukkit.Location;
+import org.bukkit.block.Chest;
+import org.bukkit.block.DoubleChest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.block.Action;
 
 import eu.carrade.amaury.BallsOfSteel.i18n.I18n;
 
@@ -67,6 +69,49 @@ public class BoSListener implements Listener {
 			ev.getPlayer().sendMessage(i.t("chests.otherTeamChest"));
 			ev.setCancelled(true);
 			
+		}
+	}
+	
+	/**
+	 * Used to prevent items to be put on or removed from the diamond chests using hoppers.
+	 * 
+	 * @param ev
+	 */
+	@EventHandler
+	public void onInventoryMoveItem(InventoryMoveItemEvent ev) {
+		Location from = null, to = null;
+		
+		if(ev.getSource().getHolder() instanceof Chest) {
+			from = ((Chest) ev.getSource().getHolder()).getLocation();
+		}
+		else if(ev.getSource().getHolder() instanceof DoubleChest) {
+			from = ((DoubleChest) ev.getSource().getHolder()).getLocation();
+		}
+		
+		if(ev.getDestination().getHolder() instanceof Chest) {
+			to = ((Chest) ev.getDestination().getHolder()).getLocation();
+		}
+		else if(ev.getDestination().getHolder() instanceof DoubleChest) {
+			to = ((DoubleChest) ev.getDestination().getHolder()).getLocation();
+		}
+		
+		// The Location returned by the DoubleChest's holders is the
+		// location of the middle of the chest.
+		// These locations cannot be compared to the stored ones.
+		// Because we store, for each double chest, the location of the two sides of the
+		// chest, we just floor the X and Z coordinates.
+		if(from != null) {
+			from.setX(from.getBlockX());
+			from.setZ(from.getBlockZ());
+		}
+		if(to != null) {
+			to.setX(to.getBlockX());
+			to.setZ(to.getBlockZ());
+		}
+		
+		if(p.getGameManager().getTrackedChests().contains(from)
+				|| p.getGameManager().getTrackedChests().contains(to)) {
+			ev.setCancelled(true);
 		}
 	}
 	
