3cdcc761bbc71867191d1a556d6a3f138bac250e
Amaury Carrade <amaury@carrade.eu>
2016-10-11 12:24:43 +0200

    Moar spheres
    - Now generating more spheres per chunk (random between 1 and the generation height divided by three times the distance between two spheres.

--- src/main/java/eu/carrade/amaury/BallsOfSteel/generation/generation/BallPopulator.java
+++ src/main/java/eu/carrade/amaury/BallsOfSteel/generation/generation/BallPopulator.java
@@ -66,49 +66,72 @@ public class BallPopulator extends BlockPopulator
         final int yMin = generationManager.getLowestCorner().getBlockY();
         final int yMax = generationManager.getHighestCorner().getBlockY();
 
-        final Location base = new Location(world, (chunk.getX() << 4) + random.nextInt(16), random.nextInt(yMax - yMin) + yMin, (chunk.getZ() << 4) + random.nextInt(16));
-        if (!this.generationManager.isInsideBoundaries(base))
-            return;
-
-        final Vector baseVector = BukkitUtil.toVector(base);
-        final com.sk89q.worldedit.world.World worldEditWorld = BukkitUtil.getLocalWorld(world);
-
         final Integer spheresFreeDistance = MapConfig.GENERATION.MAP.DISTANCE_BETWEEN_SPHERES.get();
         final Integer buildingsFreeDistance = MapConfig.GENERATION.MAP.DISTANCE_BETWEEN_SPHERES_AND_STATIC_BUILDINGS.get();
 
-        final EditSession session = WorldEdit.getInstance().getEditSessionFactory().getEditSession(worldEditWorld, -1);
+        final int spheresInThisChunk = random.nextInt((int) Math.floor(((double) (yMax - yMin)) / ((double) spheresFreeDistance * 3))) + 1;
 
-        final EllipsoidRegion noSpheresRegion = new EllipsoidRegion(worldEditWorld, baseVector, new Vector(spheresFreeDistance, spheresFreeDistance, spheresFreeDistance));
-        final EllipsoidRegion noBuildingsRegion = new EllipsoidRegion(worldEditWorld, baseVector, new Vector(buildingsFreeDistance, buildingsFreeDistance, buildingsFreeDistance));
+        final com.sk89q.worldedit.world.World worldEditWorld = BukkitUtil.getLocalWorld(world);
+        final EditSession session = WorldEdit.getInstance().getEditSessionFactory().getEditSession(worldEditWorld, -1);
 
         session.enableQueue();
         session.setFastMode(false);
 
-
-        // We check if any sphere is too close
-        for (final Vector nearChunk : noSpheresRegion.getChunkCubes())
+        spheresLoop:
+        for (int i = 0; i < spheresInThisChunk; i++)
         {
-            final ChunkSnapshot snapshot = world.getChunkAt(nearChunk.getBlockX(), nearChunk.getBlockZ()).getChunkSnapshot(false, false, false);
-            if (!snapshot.isSectionEmpty(Math.min(Math.max(nearChunk.getBlockY(), 0), 15)))
-                return;
+            // Determines where the sphere should be generated
+            final int generationWindowHeight = (int) (Math.floor((double) (yMax - yMin)) / ((double) spheresInThisChunk));
+            final int localYMin = i * generationWindowHeight;
+            final int localYMax = localYMin + generationWindowHeight;
+
+            // Finds a random point in the chunk section
+            final Location base = new Location(world, (chunk.getX() << 4) + random.nextInt(16), random.nextInt(localYMax - localYMin) + localYMin, (chunk.getZ() << 4) + random.nextInt(16));
+            if (!this.generationManager.isInsideBoundaries(base)) continue;
+
+            final Vector baseVector = BukkitUtil.toVector(base);
+
+
+            // Proximity checks
+
+            final EllipsoidRegion noSpheresRegion = new EllipsoidRegion(worldEditWorld, baseVector, new Vector(spheresFreeDistance, spheresFreeDistance, spheresFreeDistance));
+            final EllipsoidRegion noBuildingsRegion = new EllipsoidRegion(worldEditWorld, baseVector, new Vector(buildingsFreeDistance, buildingsFreeDistance, buildingsFreeDistance));
+
+
+            // We check if any sphere is too close
+            for (final Vector nearChunk : noSpheresRegion.getChunkCubes())
+            {
+                final ChunkSnapshot snapshot = world.getChunkAt(nearChunk.getBlockX(), nearChunk.getBlockZ()).getChunkSnapshot(false, false, false);
+                if (!snapshot.isSectionEmpty(Math.min(Math.max(nearChunk.getBlockY(), 0), 15)))
+                {
+                    continue spheresLoop;
+                }
+            }
+
+            // We also check if this position is correct regarding the static buildings private zones
+            for (CuboidRegion privateBuildingRegion : generationManager.getBuildingsPrivateRegions())
+            {
+                if (GeometryUtils.intersects(privateBuildingRegion, noBuildingsRegion))
+                {
+                    continue spheresLoop;
+                }
+            }
+
+
+            // Generation
+            final GenerationProcess process = this.generationManager.getRandomGenerationProcess(random);
+            process.applyAt(base, random, session);
+
+
+            // Ensures all the blocks are wrote, as the populator checks for other spheres existence to
+            // generate (or not).
+            session.flushQueue();
+
+
+            if (this.generationManager.isLogged())
+            {
+                PluginLogger.info("Sphere {0} generated at {1} in {2} ms", process.getName(), base.toVector(), System.currentTimeMillis() - time);
+            }
         }
-
-        // We also check if this position is correct regarding the static buildings private zones
-        for (CuboidRegion privateBuildingRegion : generationManager.getBuildingsPrivateRegions())
-            if (GeometryUtils.intersects(privateBuildingRegion, noBuildingsRegion))
-                return;
-
-
-        final GenerationProcess process = this.generationManager.getRandomGenerationProcess(random);
-
-        process.applyAt(base, random, session);
-
-        // Ensures all the blocks are wrote, as the populator checks for other spheres existence to
-        // generate (or not).
-        session.flushQueue();
-
-
-        if (this.generationManager.isLogged())
-            PluginLogger.info("Sphere {0} generated at {1} in {2} ms", process.getName(), base.toVector(), System.currentTimeMillis() - time);
     }
 }
